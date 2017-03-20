dfcd8e5450f16e77f787a8d5aa1fffa050d62c7e
Brandon <brandonsung92@gmail.com>
2017-03-20 18:27:54 +0800

    Move methods to prototype for controls

--- js/controls/FPSControls.js
+++ js/controls/FPSControls.js
@@ -1,31 +1,39 @@
 const THREE = require('three');
 const EventHandler = require('../tools/EventHandler.js');
 
-const ThreeFPSControls = function(camera, collisionDetection, sensitivity, movespeed, rateOfFire, afterFire) {
+const FPSControls = function(camera, collisionDetection, sensitivity, movespeed, rateOfFire, afterFire) {
+    this.movespeed = movespeed;
 
-    // Used by ObjectManager
-    this.adjustTimers = function(time) {
-        this.prevUpdateTime += time;
-        this.prevFireTime += time;
+    let degreesPerDot = 0.022; // Multiplier used in CSGO
+
+    this.sensMultiplier = (degreesPerDot * sensitivity) * (Math.PI / 180);
+
+    this.collisionDetection = collisionDetection;
+    this.moveState = {
+        forward: false,
+        left: false,
+        back: false,
+        right: false
     };
 
-    // Used by ObjectManager
-    this.toggle = function(running) {
-        this.running = running;
-    };
+    this.prevUpdateTime = performance.now();
 
-    // Used by ObjectManager
-    this.dispose = function() {
-        this.eventHandler.removeListeners();
-    };
+    this.pitchObject = new THREE.Object3D();
+    this.yawObject = new THREE.Object3D();
 
-    // Used by ObjectManager
-    this.update = function() {
-        this.updatePosition();
-        this.updateFireState();
-    };
+    camera.rotation.set(0, 0, 0);
+    this.pitchObject.add(camera);
+    this.yawObject.add(this.pitchObject);
 
-    this.setupListeners = function() {
+    this.rateOfFire = rateOfFire;
+    this.afterFire = afterFire;
+
+    this.prevFireTime = performance.now();
+    this.firing = false;
+
+    this.running = true;
+
+    let setupListeners = function() {
         if (!this.eventHandler) this.eventHandler = new EventHandler();
         this.eventHandler.setListeners([
             {
@@ -102,120 +110,106 @@ const ThreeFPSControls = function(camera, collisionDetection, sensitivity, moves
         ]);
         this.eventHandler.setupListeners();
     };
-
-    this.addTo = function(scene) {
-        scene.add(this.yawObject);
-    };
-
-    this.getMoveState = function() {
-        return this.moveState;
-    };
-
-    this.getYawRotation = function() {
-        return this.yawObject.rotation;
-    };
-
-    this.getPitchRotation = function() {
-        return this.pitchObject.rotation;
-    };
-
-    this.getDirection = function() {
-        let pitchRotation = this.getPitchRotation();
-        let yawRotation = this.getYawRotation();
-
-        let direction = new THREE.Vector3(0, 0, -1);
-        direction.applyEuler(pitchRotation).applyEuler(yawRotation);
-
-        return direction;
-    };
-
-    this.getPosition = function() {
-        return this.yawObject.position;
-    };
-
-    this.setPosition = function(x, y ,z) {
-        this.yawObject.position.set(x, y, z);
-    };
-
-    this.getRaycaster = function() {
-        let caster = new THREE.Raycaster();
-        caster.set(this.getPosition(), this.getDirection());
-        return caster;
-    };
-
-    // this needs to be called in game's animate method
-    this.updatePosition = function(collisionDetection) {
-        let time = performance.now();
-        let timePassed = (time - this.prevUpdateTime) / 1000;
-
-        let direction = new THREE.Vector3(0, 0, 0);
-
-        let adjustedMoveState = this.collisionDetection.getAdjustedMoveState(
-            this.moveState,
-            this.getPosition(),
-            this.getYawRotation()
-        );
-
-        let {forward, left, back, right} = adjustedMoveState;
-        if (forward) direction.add(new THREE.Vector3(0, 0, -1));
-        if (left) direction.add(new THREE.Vector3(-1, 0, 0));
-        if (back) direction.add(new THREE.Vector3(0, 0, 1));
-        if (right) direction.add(new THREE.Vector3(1, 0, 0));
-
-        if (direction.length() != 0) {
-            direction.normalize();
-            this.yawObject.translateOnAxis(direction, this.movespeed * timePassed);
-        }
-
-        this.prevUpdateTime = time;
-    }
-
-    this.updateFireState = function() {
-        if (!this.firing) return;
-
-        let time = performance.now();
-
-        let shotDue = (time - this.prevFireTime) > ((1 / this.rateOfFire) * 1000);
-        if (shotDue) {
-            this.prevFireTime = time;
-            if (this.afterFire) {
-                this.afterFire(this.getRaycaster());
-            }
-        }
-    };
-
-    this.movespeed = movespeed;
-
-    let degreesPerDot = 0.022; // Multiplier used in CSGO
-
-    this.sensMultiplier = (degreesPerDot * sensitivity) * (Math.PI / 180);
-
-    this.collisionDetection = collisionDetection;
-    this.moveState = {
-        forward: false,
-        left: false,
-        back: false,
-        right: false
-    };
-
-    this.prevUpdateTime = performance.now();
-
-    this.pitchObject = new THREE.Object3D();
-    this.yawObject = new THREE.Object3D();
-
-    camera.rotation.set(0, 0, 0);
-    this.pitchObject.add(camera);
-    this.yawObject.add(this.pitchObject);
-
-    this.rateOfFire = rateOfFire;
-    this.afterFire = afterFire;
-
-    this.prevFireTime = performance.now();
-    this.firing = false;
-
-    this.running = true;
-
-    this.setupListeners();
+    setupListeners.call(this);
 };
 
-module.exports = ThreeFPSControls;
+FPSControls.prototype.adjustTimers = function(time) {
+    this.prevUpdateTime += time;
+    this.prevFireTime += time;
+};
+
+FPSControls.prototype.toggle = function(running) {
+    this.running = running;
+};
+
+FPSControls.prototype.dispose = function() {
+    this.eventHandler.removeListeners();
+};
+
+FPSControls.prototype.update = function() {
+    this.updatePosition();
+    this.updateFireState();
+};
+
+FPSControls.prototype.addTo = function(scene) {
+    scene.add(this.yawObject);
+};
+
+FPSControls.prototype.getMoveState = function() {
+    return this.moveState;
+};
+
+FPSControls.prototype.getYawRotation = function() {
+    return this.yawObject.rotation;
+};
+
+FPSControls.prototype.getPitchRotation = function() {
+    return this.pitchObject.rotation;
+};
+
+FPSControls.prototype.getDirection = function() {
+    let pitchRotation = this.getPitchRotation();
+    let yawRotation = this.getYawRotation();
+
+    let direction = new THREE.Vector3(0, 0, -1);
+    direction.applyEuler(pitchRotation).applyEuler(yawRotation);
+
+    return direction;
+};
+
+FPSControls.prototype.getPosition = function() {
+    return this.yawObject.position;
+};
+
+FPSControls.prototype.setPosition = function(x, y ,z) {
+    this.yawObject.position.set(x, y, z);
+};
+
+FPSControls.prototype.getRaycaster = function() {
+    let caster = new THREE.Raycaster();
+    caster.set(this.getPosition(), this.getDirection());
+    return caster;
+};
+
+// this needs to be called in game's animate method
+FPSControls.prototype.updatePosition = function(collisionDetection) {
+    let time = performance.now();
+    let timePassed = (time - this.prevUpdateTime) / 1000;
+
+    let direction = new THREE.Vector3(0, 0, 0);
+
+    let adjustedMoveState = this.collisionDetection.getAdjustedMoveState(
+        this.moveState,
+        this.getPosition(),
+        this.getYawRotation()
+    );
+
+    let {forward, left, back, right} = adjustedMoveState;
+    if (forward) direction.add(new THREE.Vector3(0, 0, -1));
+    if (left) direction.add(new THREE.Vector3(-1, 0, 0));
+    if (back) direction.add(new THREE.Vector3(0, 0, 1));
+    if (right) direction.add(new THREE.Vector3(1, 0, 0));
+
+    if (direction.length() != 0) {
+        direction.normalize();
+        this.yawObject.translateOnAxis(direction, this.movespeed * timePassed);
+    }
+
+    this.prevUpdateTime = time;
+}
+
+FPSControls.prototype.updateFireState = function() {
+    if (!this.firing) return;
+
+    let time = performance.now();
+
+    let shotDue = (time - this.prevFireTime) > ((1 / this.rateOfFire) * 1000);
+    if (shotDue) {
+        this.prevFireTime = time;
+        if (this.afterFire) {
+            this.afterFire(this.getRaycaster());
+        }
+    }
+};
+
+module.exports = FPSControls;
